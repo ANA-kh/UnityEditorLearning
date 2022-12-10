980306785aba9f65006528fa9a8273af2fa84f3c
ANA_h <1154073432@qq.com>
2022-12-10 20:20:25 +0800

    add add CustomAttribute for getting custom EditorWindow

--- EditorExtensionProject/Assets/EditorFramework/Editor/RootWindow.cs
+++ EditorExtensionProject/Assets/EditorFramework/Editor/RootWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEditor;
@@ -8,6 +9,10 @@ namespace EditorFramework
 {
     public class RootWindow : EditorWindow
     {
+        private IEnumerable<Type> _editorWindowTypes;
+        private Vector2 _scrollPosition;
+        private IEnumerable<Type> _customEditorWindowTypes;
+
         [MenuItem("EditorFramework/Open %#E")]
         static void Open()
         {
@@ -17,16 +22,44 @@ namespace EditorFramework
         private void OnEnable()
         {
             var editorWindowType = typeof(EditorWindow);
-            var m_Parent = editorWindowType.GetField("m_Parent", BindingFlags.NonPublic | BindingFlags.Instance);
+            //var m_Parent = editorWindowType.GetField("m_Parent", BindingFlags.NonPublic | BindingFlags.Instance);
 
-            var editorWindowTypes = AppDomain.CurrentDomain.GetAssemblies()
+            _editorWindowTypes = AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(assembly => assembly.GetTypes())
                 .Where(type => type.IsSubclassOf(editorWindowType));
-            
-            foreach (var windowType in editorWindowTypes)
+            _customEditorWindowTypes = _editorWindowTypes.Where(type =>
+                type.GetCustomAttribute(typeof(CustomEditorWindowAttribute), true) != null);
+        }
+
+        private void OnGUI()
+        {
+            foreach (var customEditorWindowType in _customEditorWindowTypes)
             {
-                Debug.Log(windowType.Name);
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label(customEditorWindowType.Name);
+                    if (GUILayout.Button("Open", GUILayout.Width(100)))
+                    {
+                        GetWindow(customEditorWindowType).Show();
+                    }
+                }
+                GUILayout.EndHorizontal();
             }
+            GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(5));
+            _scrollPosition = GUILayout.BeginScrollView(_scrollPosition);
+            foreach (var editorWindowType in _editorWindowTypes)
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label(editorWindowType.Name);
+                    if (GUILayout.Button("Open", GUILayout.Width(100)))
+                    {
+                        GetWindow(editorWindowType).Show();
+                    }
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
         }
     }
 }
---
+++ EditorExtensionProject/Assets/EditorFramework/Example/1.CustomEditor/Editor/CustomEditorExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorFramework.Example._1.CustomEditor.Editor
+{
+    [CustomEditorWindow]
+    public class CustomEditorExample: EditorWindow
+    {
+        private void OnGUI()
+        {
+            GUILayout.Label("This is a Custom Editor Window");
+        }
+    }
+}
