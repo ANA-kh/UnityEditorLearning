67c962fb334ec50e141cd0c6098525ce6c495621
ANA_h <1154073432@qq.com>
2022-12-12 12:10:06 +0800

    add [CustomEditor]和OnInspectorGUI 定义单一脚本的绘制

---
+++ EditorExtensionProject/Assets/EditorExtensions/3. InspectorExample/2.CustomEditor/CustomEditorExample.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace EditorExtensions
+{
+    public class CustomEditorExample : MonoBehaviour
+    {
+        public float Hp;
+        public float Range;
+        public string RoleName;
+        public GameObject OtherObj;
+    }
+}
---
+++ EditorExtensionProject/Assets/EditorExtensions/3. InspectorExample/2.CustomEditor/Editor/CustomEditorExampleInspector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    [CustomEditor(typeof(CustomEditorExample))]
+    public class CustomEditorExampleInspector : Editor
+    {
+        CustomEditorExample Target
+        {
+            get { return target as CustomEditorExample; }//target : The object being inspected.
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var hpRect = GUILayoutUtility.GetRect(18, 18, "TextField");
+            EditorGUI.ProgressBar(hpRect,Target.Hp, "Hp");
+            var rangeRect = GUILayoutUtility.GetRect(18, 18, "TextField");
+            EditorGUI.ProgressBar(rangeRect, Target.Range, "Range");
+            
+            Target.RoleName = EditorGUILayout.TextField("RoleName", Target.RoleName);
+            
+            EditorGUILayout.ObjectField(serializedObject.FindProperty("OtherObj"));
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
