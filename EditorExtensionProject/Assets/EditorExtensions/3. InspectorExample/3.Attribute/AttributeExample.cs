ad9ede2fc1d7056400df043e20830a716fa82abc
ANA_h <1154073432@qq.com>
2022-12-12 15:15:27 +0800

    add Inspector(ContextMenu CustomEditor Attribute ObjectPreview) Practice

---
+++ EditorExtensionProject/Assets/EditorExtensions/3. InspectorExample/3.Attribute/AttributeExample.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace EditorExtensions
+{
+    /// <summary>
+    /// Attributes文档 https://docs.unity3d.com/2021.3/Documentation/ScriptReference/AddComponentMenu.html
+    /// </summary>
+    
+    [HelpURL("https://docs.unity3d.com/2021.3/Documentation/ScriptReference/AddComponentMenu.html")]
+    public class AttributeExample : MonoBehaviour
+    {
+        public int Age;
+
+        [HideInInspector]
+        public int HideAge;
+        
+        private int _priAge;
+        
+        [Header("年龄")]
+        [SerializeField]
+        private int _SerializeFieldAge;
+
+        [Space(20)]
+        public int Space = 20;
+        
+        [Multiline(5)]
+        public string Multiline = "多行文本";
+        
+        [TextArea(5, 10)]
+        public string TextArea = "文本区域";
+        
+        [Range(0, 100)]
+        public int Range = 0;
+        
+        [My]
+        public int My = 0;
+    }
+}
---
+++ EditorExtensionProject/Assets/EditorExtensions/3. InspectorExample/3.Attribute/MyAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    public class MyAttribute : PropertyAttribute { }
+}
