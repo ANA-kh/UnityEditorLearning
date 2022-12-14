5f9c3bb0e1d1565bfb77811d67889ace9188175a
ANA_h <1154073432@qq.com>
2022-12-14 12:09:39 +0800

    add GenericMenuExample

---
+++ EditorExtensionProject/Assets/EditorExtensions/11.UndoExample/Editor/UndoExample.cs
@@ -0,0 +1,69 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    public class UndoExample
+    {
+        private const string PathRoot = "EditorExtensions/07.Undo/";
+
+        [MenuItem(PathRoot + "Create Object")]
+        static void CreateObject()
+        {
+            var go = new GameObject("New Object");
+            Undo.RegisterCreatedObjectUndo(go, "Create Object");
+        }
+
+        [MenuItem(PathRoot + "Move Object")]
+        public static void Move()
+        {
+            var trans = Selection.activeGameObject.transform;
+            if (trans)
+            {
+                Undo.RecordObject(trans,"MoveObj");
+                trans.position += Vector3.up;
+            }
+        }
+        
+        [MenuItem(PathRoot + "Add Component")]
+        public static void AddComponent()
+        {
+            var go = Selection.activeGameObject;
+            if (go)
+            {
+                Undo.AddComponent(go,typeof(Rigidbody));
+            }
+        }
+        
+        [MenuItem(PathRoot + "Destroy Object")]
+        public static void Destroy()
+        {
+            var go = Selection.activeGameObject;
+            if (go)
+            {
+                Undo.DestroyObjectImmediate(go);
+            }
+        }
+        
+        [MenuItem(PathRoot + "SetParent Object")]
+        public static void SetParent()
+        {
+            var trans = Selection.activeGameObject.transform;
+            var camera = Camera.main.transform;
+            
+            if (trans)
+            {
+                Undo.SetTransformParent(trans,camera,trans.name);
+            }
+        }
+        
+        [MenuItem(PathRoot + "Move Object",true)]
+        [MenuItem(PathRoot + "Add Component",true)]
+        [MenuItem(PathRoot + "Destroy Object",true)]
+        [MenuItem(PathRoot + "SetParent Object",true)]
+        public static bool Check()
+        {
+            return Selection.activeGameObject != null;
+        }
+    }
+}
---
+++ EditorExtensionProject/Assets/EditorExtensions/12.GenericMenuExample/Editor/GenericMenuExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    /// <summary>
+    /// https://docs.unity.cn/cn/current/ScriptReference/GenericMenu.html
+    /// </summary>
+    public class GenericMenuExample : EditorWindow
+    {
+        [MenuItem("EditorExtensions/08.GenericMenu/Open")]
+        static void Open()
+        {
+            GetWindow<GenericMenuExample>().Show();
+        }
+
+        private void OnGUI()
+        {
+            var e = Event.current;
+            if (e.type == EventType.MouseDown && e.button == 1)
+            {
+                var menu = new GenericMenu();
+                menu.AddItem(new GUIContent("Item1"), false, () => { Debug.Log("Item1"); });
+                menu.AddItem(new GUIContent("Items/Item2"), false, () => { Debug.Log("Item2"); });
+                menu.AddItem(new GUIContent("Items/Item3"), false, () => { Debug.Log("Item3"); });
+                menu.AddSeparator("Items/");
+                menu.AddItem(new GUIContent("Items/Item4"), false, () => { Debug.Log("Item4"); });
+                menu.ShowAsContext();
+                e.Use();
+            }
+        }
+    }
+}
